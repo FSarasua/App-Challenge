23964db5c2917615e50ab76ba8a7c4e5cc3c0242
Francisco Javier Sarasua Galan <fraciscojavier.sg.trabajo@gmail.com>
2021-04-17 17:21:07 +0200

    First commit
    Initial configuration: Added App Icon to Assets, new image "Logo" and updated color "AccentColor". Configure LaunchScreen and AppDelegate. Changed initial flow navigation screen with programatically flow navigation from AppDelegate.
    
    Deleted files that will not be used during this challenge: Main.storyboard and SceneDelegate.swift.
    
    Folder hierarchy created. Structured in the following points:
    
    - Configuration: Files that build the initial base of the project.
    - Database: Files that compose the local database of the project.
    - Resources: Useful files such as fonts, images, etc.
    - Modules: Main section of the application that contains the files that represent the screens, data management, navigation flow, etc.

---
+++ Apps Challenge/Configuration/AppDelegate.swift
@@ -0,0 +1,61 @@
+//
+//  AppDelegate.swift
+//  Apps Challenge
+//
+//  Created by Francisco Javier Sarasua Galan on 17/04/2021.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow? = nil
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let navigation = UINavigationController(rootViewController: UIViewController())
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = navigation
+            
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "Apps_Challenge")
+        
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            
+            if let error = error as NSError? {
+                
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        
+        let context = persistentContainer.viewContext
+        
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                
+                let nserror = error as NSError
+                
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
